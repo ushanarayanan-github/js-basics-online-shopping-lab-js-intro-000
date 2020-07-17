89dae8cc63318c6f958099a0e8b62aac604e4b8d
creative-buffer-9578 <cmuusha@gmail.com>
2020-07-17 04:05:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,7 +48,13 @@ return sum
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i<cart.length;i++){
+  if (item == cart[i].itemName) {
+    cart.splice(i,1)
+    return cart
+  }
+ }
+ return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
